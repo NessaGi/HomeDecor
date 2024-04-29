0884531638c28d88d5d8ce601be9111778c5bb9a
Vanessa <nessa_g@hotmail.fr>
2024-04-29 13:21:44 +0200

    Footer

---
+++ Home-decor-app/src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy;Contenu de Vanessa Girondin</p>
+      <p>contactez moi</p>
+    </footer>
+  );
+};
+
+export default Footer;
