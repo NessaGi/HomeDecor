845c753c8f72131af78d2e72ba45a81cc2dc4e12
Vanessa <nessa_g@hotmail.fr>
2024-04-29 13:06:12 +0200

    ajout composant nav

---
+++ Home-decor-app/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Articles</a></li>
+        <li><a href="#">À propos de nous</a></li>
+        <li><a href="#">Contactez nous</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
